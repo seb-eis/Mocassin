34422fca573899f3d349a5f57251f01600472437
Sebastian Eisele <sebastian.eisele@rwth-aachen.de>
2019-02-27 16:44:43 +0100

    Added cluster bsub build jobs for westmere and beckton

--- clusterbuild.sh
+++ clusterbuild.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
+cpu_name=$1
 make_flags="-DCMAKE_BUILD_TYPE=Release"
 
 base_target="$HOME/Mocassin/Simulator"
-gcc_target="$base_target/gcc_release"
-icc_target="$base_target/icc_release"
+gcc_target="$base_target/gcc_release_$cpu_name"
+icc_target="$base_target/icc_release_$cpu_name"
 build_source="$base_target/"
 
 rm -rf $gcc_target
@@ -13,7 +14,7 @@ rm -rf $icc_target
 mkdir $gcc_target
 mkdir $icc_target
 
-echo "Building gcc release ..\n"
+echo "Building $cpu_name gcc release ..\n"
 cd $gcc_target
 CC=gcc cmake $make_flags $base_target
 make
@@ -23,6 +24,6 @@ echo "Building icc release ..\n"
 cd $icc_target
 CC=icc cmake $make_flags $base_target
 make
-echo "Building icc release finished"
+echo "Building $cpu_name icc release finished"
 
 cd $HOME
