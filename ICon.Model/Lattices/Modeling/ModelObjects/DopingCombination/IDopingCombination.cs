4f17dcdeb45e5cc33c72c9a0853629f9e0577534
john <john.arnold@rwth-aachen.de>
2019-04-08 12:09:30 +0200

    Added DopingCombination back into the project.

---
+++ ICon.Model/Lattices/Modeling/ModelObjects/DopingCombination/IDopingCombination.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mocassin.Model.Basic;
+using Mocassin.Model.Particles;
+using Mocassin.Model.Structures;
+
+namespace Mocassin.Model.Lattices
+{
+    /// <summary>
+    /// Set of element indexes and sublattice index for doping information
+    /// </summary>
+    public interface IDopingCombination : IModelObject
+    {
+
+        /// <summary>
+        /// Dopand particle
+        /// </summary>
+        IParticle Dopant { get; }
+
+        /// <summary>
+        /// Particle that is doped
+        /// </summary>
+        IParticle Dopable { get; }
+
+        /// <summary>
+        /// unit cell position (contains information about the sublattice)
+        /// </summary>
+        IUnitCellPosition UnitCellPosition { get; }
+
+    }
+}
