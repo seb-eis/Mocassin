0def6c3c87710a88fe57349d46048bb0f0fffdc5
Sebastian Eisele <sebastian.eisele@rwth-aachen.de>
2019-04-17 17:07:44 +0200

    Added MMC job package and description creation UI elemnts

--- Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/MmcJobDescriptionControlViewModel.cs
+++ Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/MmcJobDescriptionControlViewModel.cs
@@ -13,6 +13,58 @@ namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslati
         /// </summary>
         private MmcJobDescriptionGraph JobDescription { get; }
 
+        /// <summary>
+        ///     Get or set the abort tolerance value on the target <see cref="MmcJobDescriptionGraph" />
+        /// </summary>
+        public double? BreakTolerance
+        {
+            get => double.TryParse(JobDescription.BreakTolerance, out var x) ? x : (double?) null;
+            set
+            {
+                JobDescription.BreakTolerance = value?.ToString();
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Get or set the abort sequence length on the target <see cref="MmcJobDescriptionGraph" />
+        /// </summary>
+        public int? BreakSampleLength
+        {
+            get => int.TryParse(JobDescription.BreakSampleLength, out var x) ? x : (int?) null;
+            set
+            {
+                JobDescription.BreakSampleLength = value?.ToString();
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Get or set the abort sample interval on the target <see cref="MmcJobDescriptionGraph" />
+        /// </summary>
+        public int? BreakSampleInterval
+        {
+            get => int.TryParse(JobDescription.BreakSampleInterval, out var x) ? x : (int?) null;
+            set
+            {
+                JobDescription.BreakSampleInterval = value?.ToString();
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Get or set the result sample mcs on the target <see cref="MmcJobDescriptionGraph" />
+        /// </summary>
+        public int? ResultSampleMcs
+        {
+            get => int.TryParse(JobDescription.ResultSampleMcs, out var x) ? x : (int?) null;
+            set
+            {
+                JobDescription.ResultSampleMcs = value?.ToString();
+                OnPropertyChanged();
+            }
+        }
+
         /// <inheritdoc />
         public MmcJobDescriptionControlViewModel(MmcJobDescriptionGraph jobDescription)
             : base(jobDescription)
---
+++ Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/MmcJobDescriptionSetControlViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mocassin.UI.GUI.Controls.Base.ViewModels;
+using Mocassin.UI.Xml.Jobs;
+
+namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslation.GridControl
+{
+    /// <summary>
+    ///     The <see cref="CollectionControlViewModel{T}" /> for <see cref="MmcJobDescriptionSetControlView" /> that controls
+    ///     manipulation of <see cref="MmcJobDescriptionGraph" />
+    /// </summary>
+    public sealed class MmcJobDescriptionSetControlViewModel : CollectionControlViewModel<MmcJobDescriptionGraph>
+    {
+        /// <summary>
+        ///     Get the <see cref="MmcJobDescriptionGraph" /> that supplies the <see cref="MmcJobDescriptionGraph" /> collection
+        /// </summary>
+        public MmcJobPackageDescriptionGraph MmcJobPackage { get; }
+
+        /// <summary>
+        ///     Creates new <see cref="MmcJobDescriptionSetControlViewModel" /> for the passed
+        ///     <see cref="MmcJobPackageDescriptionGraph" />
+        /// </summary>
+        /// <param name="mmcJobPackage"></param>
+        public MmcJobDescriptionSetControlViewModel(MmcJobPackageDescriptionGraph mmcJobPackage)
+        {
+            MmcJobPackage = mmcJobPackage ?? throw new ArgumentNullException(nameof(mmcJobPackage));
+            SetCollection(mmcJobPackage.JobConfigurations);
+        }
+    }
+}
