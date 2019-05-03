d8f8fd90fbfb329891cd699ec49c7784b1f598a9
Sebastian Eisele <sebastian.eisele@rwth-aachen.de>
2019-05-03 16:45:03 +0200

    Added doping UiElements to MMC job configuration system

--- Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/Adapter/MmcJobPackageMmcJobDescriptionSetViewModelConverter.cs
+++ Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/Adapter/MmcJobPackageMmcJobDescriptionSetViewModelConverter.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Markup;
 using Mocassin.UI.Xml.Jobs;
+using Mocassin.UI.Xml.Main;
 
 namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslation.GridControl.Adapter
 {
@@ -10,7 +11,7 @@ namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslati
     ///     The <see cref="IValueConverter" /> to wrap <see cref="MmcJobPackageDescriptionGraph" /> instances into
     ///     <see cref="MmcJobDescriptionSetControlViewModel" /> instances
     /// </summary>
-    public class MmcJobPackageMmcJobDescriptionSetViewModelConverter : MarkupExtension, IValueConverter
+    public class MmcJobPackageMmcJobDescriptionSetViewModelConverter : MarkupExtension, IMultiValueConverter
     {
         /// <inheritdoc />
         public override object ProvideValue(IServiceProvider serviceProvider)
@@ -19,14 +20,15 @@ namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslati
         }
 
         /// <inheritdoc />
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is MmcJobPackageDescriptionGraph jobPackage) return new MmcJobDescriptionSetControlViewModel(jobPackage);
-            return null;
+            if (!(values[0] is MocassinProjectGraph projectGraph)) return null;
+            if (!(values[1] is MmcJobPackageDescriptionGraph packageDescription)) return null;
+            return new MmcJobDescriptionSetControlViewModel(packageDescription, projectGraph);
         }
 
         /// <inheritdoc />
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotSupportedException();
         }
--- Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/MmcJobDescriptionSetControlViewModel.cs
+++ Mocassin.UI.GUI/Controls/ProjectWorkControl/ModelControls/JobTranslation/GridControl/MmcJobDescriptionSetControlViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Mocassin.UI.GUI.Controls.Base.ViewModels;
 using Mocassin.UI.Xml.Jobs;
+using Mocassin.UI.Xml.Main;
 
 namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslation.GridControl
 {
@@ -15,14 +16,18 @@ namespace Mocassin.UI.GUI.Controls.ProjectWorkControl.ModelControls.JobTranslati
         /// </summary>
         public MmcJobPackageDescriptionGraph MmcJobPackage { get; }
 
+        public MocassinProjectGraph ProjectGraph { get; }
+
         /// <summary>
         ///     Creates new <see cref="MmcJobDescriptionSetControlViewModel" /> for the passed
         ///     <see cref="MmcJobPackageDescriptionGraph" />
         /// </summary>
         /// <param name="mmcJobPackage"></param>
-        public MmcJobDescriptionSetControlViewModel(MmcJobPackageDescriptionGraph mmcJobPackage)
+        /// <param name="projectGraph"></param>
+        public MmcJobDescriptionSetControlViewModel(MmcJobPackageDescriptionGraph mmcJobPackage, MocassinProjectGraph projectGraph)
         {
             MmcJobPackage = mmcJobPackage ?? throw new ArgumentNullException(nameof(mmcJobPackage));
+            ProjectGraph = projectGraph ?? throw new ArgumentNullException(nameof(projectGraph));
             SetCollection(mmcJobPackage.JobConfigurations);
         }
     }
