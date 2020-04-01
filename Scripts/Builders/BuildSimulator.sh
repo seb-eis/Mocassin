8140665c144f2144be2a6486799ee50eda4754ed
Sebastian <sebastian.eisele@rwth-aachen.de>
2020-04-01 18:05:37 +0200

    Change build script to LF

--- Scripts/Builders/BuildSimulator.sh
+++ Scripts/Builders/BuildSimulator.sh
@@ -4,4 +4,4 @@ module load intel/19.1
 module unload gcc
 module load gcc/9
 buildScriptPath="$HOME/Mocassin/Simulator/Scripts/Builders/MocassinBuildScript.py"
-python3.6 $buildScriptPath
+python3 $buildScriptPath
