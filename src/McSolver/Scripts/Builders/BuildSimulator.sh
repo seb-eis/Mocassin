a44490c4cface110365ea30868f6627ce889fcef
Sebastian <sebastian@ewachenfeld.de>
2020-10-20 14:07:46 +0200

    Removed no longer required old build scripts

--- src/McSolver/Scripts/Builders/BuildSimulator.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-module unload intel
-module load intel/19.1
-module unload gcc
-module load gcc/10
-buildScriptPath="$HOME/Mocassin/Simulator/Scripts/Builders/MocassinBuildScript.py"
-python3 $buildScriptPath
--- src/McSolver/Scripts/Builders/linux-build.sh
+++
@@ -1,6 +0,0 @@
-buildDir="./build/"
-cmake -B $buildDir -D"CMAKE_BUILD_TYPE=Release"
-oldPath=$PWD
-cd $buildDir
-make
-cd $oldPath
